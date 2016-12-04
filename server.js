e00a56ac4d809711ce1f6b57e862c1ffbea6bc1c
Michele Ferretti <mferretti@Micheles-iMac.local>
2016-12-04 13:36:13 +0000

    sending data to arduino via serialport

--- server.js
+++ server.js
@@ -5,17 +5,24 @@ var io      = require('socket.io').listen(server);
 var serialport = require('serialport');// include the library
 
 server.listen(8080);
-
 app.use(express.static(__dirname + '/public'));
 
+SerialPort = serialport.SerialPort; // make a local instance of it
+// get port name from the command line:
+portName = process.argv[2];
+// Then you open the port using new() like so:
+var myPort = new SerialPort(portName, {
+baudRate: 9600,
+// look for return and newline at the end of each data packet:
+parser: serialport.parsers.readline("\n")
+});
 
 io.on('connection', function(socket){
   console.log('a user connected');
 
     socket.on('newGeoJSONtoDraw', function(data){
-      console.log(data);
-      
-    });
+      console.log("Sending data to serial:" + JSON.stringify(data));
+      myPort.write(data, 'utf-8');
+   }); //close data incoming event
 
-
-});
+}); //close ws stream
